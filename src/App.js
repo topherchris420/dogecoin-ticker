825d2a73e2bf3ada4fcbac8ca8e1325e2640c602
Christopher <93827718+topherchris420@users.noreply.github.com>
2022-08-22 11:03:11 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,15 +25,15 @@ const App = () => {
   return (
     <div className="App">
       <img src={logo} width={150} height={150} alt="Dogecoin Logo" />
-      <h1 className="title">Live Dogecoin Price</h1>
-      <h5 className="subtitle">Dogecoin To The Moon 🚀🌕</h5>
+      <h1 className="title">Live Dogecoin Price waitin to Bust</h1>
+      <h5 className="subtitle">To The Moon 🚀🌕</h5>
       <div className="prices-container">
         <PriceCard type="low" price={ticker.low} />
         <PriceCard type="high" price={ticker.high} />
         <PriceCard type="current" price={ticker.last} />
       </div>
       <p>
-        Dogecoin price updated every 10 seconds from{' '}
+        Dogecoin price farts every 10 seconds from{' '}
         <a href="https://wazirx.com/">WazirX API</a>
       </p>
     </div>
